81e1bc096fafca22f5c3405ef943e499c084237c
Leigh Garbs <leighgarbs@gmail.com>
2017-12-01 22:33:22 -0500

    Work in progress

--- 2017/01/aoc1.cpp
+++ 2017/01/aoc1.cpp
@@ -1,5 +1,16 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 int main(int argc, char** argv)
 {
+    // Open the input file
+    std::ifstream inputfile("input.txt");
+
+    // Read in the input data
+    std::string inputdata;
+    inputfile >> inputdata;
+
+    //std::cout << inputdata << "\n";
+    std::cout << inputdata.size() << "\n";
 }
