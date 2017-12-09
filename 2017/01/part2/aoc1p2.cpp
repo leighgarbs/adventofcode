9830ef76960d6b2040658ea6ad269cd83e9fa798
Leigh Garbs <leighgarbs@gmail.com>
2017-12-09 13:44:56 -0500

    Initialized part 2, content is still part 1

---
+++ 2017/01/part2/aoc1p2.cpp
@@ -0,0 +1,43 @@
+// Calculates solution to Advent of Code 2017 Day 1
+
+#include <cstdio>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    // Open the input file
+    std::FILE* file_id = std::fopen("../input.txt", "r");
+
+    unsigned int sum = 0;
+
+    // Prime the loop by grabbing the first character
+    unsigned int digit_current;
+    std::fscanf(file_id, "%1u", &digit_current);
+
+    // Save the first digit, we need it at the end
+    unsigned int digit_first = digit_current;
+
+    int digit_next = 0;
+
+    // Keep looping until there are no more digits
+    while (std::fscanf(file_id, "%1u", &digit_next) != EOF)
+    {
+        if (digit_current == digit_next)
+        {
+            sum += digit_current;
+        }
+
+        digit_current = digit_next;
+    }
+
+    // Handle the wrap-around at the end as a special case
+    if (digit_first == digit_current)
+    {
+        sum += digit_current;
+    }
+
+    // Print the sum
+    std::cout << sum << "\n";
+
+    return 0;
+}
